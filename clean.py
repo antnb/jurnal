0a029738cecf128c0615e0188a487d12dad26876
rosarij <antonnurbowo@gmail.com>
2024-02-15 18:44:41 +0700

    gjghjghjhgjhgjghjgh

---
+++ clean.py
@@ -0,0 +1,16 @@
+import re
+
+# Buka file teks
+with open('urls.txt', 'r') as file:
+    lines = file.readlines()
+
+# Pola untuk mencocokkan URL dengan download id
+pattern = r'https:\/\/ojs\.unud\.ac\.id\/index\.php\/kerthasemaya\/article\/view\/\d+\/\d+'
+
+# Filter baris yang tidak mengandung download id
+filtered_lines = [line for line in lines if not re.search(pattern, line)]
+
+# Tulis kembali ke file teks
+with open('kerthasemaya.txt', 'w') as file:
+    file.writelines(filtered_lines)
+
